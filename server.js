f5cb4402f6676cbcfe1d289a9f3bc42050ab0929
Abhishek Anandwade <abhi.anandwade@gmail.com>
2022-05-08 15:02:00 +0530

    first

---
+++ server.js
@@ -0,0 +1,89 @@
+//importing
+import express from 'express';
+import mongoose from 'mongoose';
+import Messages from './dbMessages.js';
+import Pusher from 'pusher';
+import cors from 'cors';
+
+//app config
+const app = express();
+const port = process.env.PORT || 9000;
+
+const pusher = new Pusher({
+    appId: "1403362",
+    key: "cf49a72316f36e1797c7",
+    secret: "026591a0e40ce8add5de",
+    cluster: "ap2",
+    useTLS: true
+  });
+
+//Middlewares
+app.use(express.json());
+app.use(cors());
+
+
+//DB config 
+const connection_url ='mongodb+srv://abhishek:7w27PFaMa4IJ7Pfz@cluster0.yfyem.mongodb.net/whatsappdb?retryWrites=true&w=majority';
+mongoose.connect(connection_url);
+
+const db = mongoose.connection;
+
+db.once("open", () =>{
+    
+ console.log('DB is connected');
+ const msgCollection = db.collection('messagecontents');
+ const changeStream = msgCollection.watch();
+
+ changeStream.on('change', (change) =>{
+     console.log("A Change occured",change);
+
+     if (change.operationType === 'insert'){
+            const messageDetails = change.fullDocument;
+            pusher.trigger('messages', 'inserted', {
+                name: messageDetails.name,
+                message: messageDetails.message,
+                timestamp: messageDetails.timestamp,
+                received: messageDetails.received
+                
+            });
+     }
+
+     else{
+            console.log('Error triggering the pusher');
+     }
+ });
+
+
+});
+
+
+//API Routs
+app.get('/', (req, res) => res.status(200).send('Hello World'));
+
+app.get('/messages/sync',(req, res) => {
+    Messages.find((err,data)=>{
+        if(err){
+            res.status(500).send(err);
+        }else{
+            res.status(200).send(data);
+        }
+    })
+})
+
+app.post('/messages/new', (req, res) => {
+    const dbMessage = req.body
+
+    Messages.create(dbMessage, (err, data) => {
+    if(err){
+        res.status(500).send(err);
+    }else{
+        res.status(200).send(data);
+   }    
+    
+    });
+
+    
+});
+
+//listeners
+app.listen(port, () => console.log(`Server started on port ${port}`));
